3cc212c7e2299e130237634d905ed45d0a672b33
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-31 22:14:07 +0800

    Import cors, cookieParser package. Also import the credentials, errHandler middleware and corsOption config as well as the customErr helper. Input the uncaughtException err handler at the top of the server this handle the synchronous errors and unhandledRejection at the bottom of the server this handle the promise error. Then also input a default route if origninal url cannot be found

--- app.js
+++ app.js
@@ -1,15 +1,76 @@
 import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import credentials from "./middleware/credentials.js";
+import corsOption from "./config/corsOptions.js";
+import CustomErr from "./helper/customErr.js";
+import errHandler from "./middleware/errorHandler.js";
 
+dotenv.config();
 const app = express();
 
+//handles syncchronous error. Ex. undifined variable or function
+//it should shut down the app because the server already crashed
+//it should invoke before the app express() to make sure it work
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("Uncaught Exception occured!, shutting down");
+
+  process.exit(1);
+})
+
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials)
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOption))
+
+// middleware json
+app.use(express.json())
+
+// other body request
+app.use(express.urlencoded({extended: true}))
+
+// middleware for cookies
+app.use(cookieParser());
+
+//api routes here!
 app.get("/", (req, res, next) => {
   res.send("HELLO")
 });
 
 
+// default route
+// handles error if the url cannot find
+app.all(/(.*)/, (req, res, next) => {
+  const err = new CustomErr(`Can't find ${req.originalUrl} on the server`, 404)
+  next(err)
+});
+
+// errorHandler middleware
+app.use(errHandler)
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log("Server listening to PORT " + PORT);
 });
+
+//handle rejection promise, if no catch block to a promise
+//it should shut down the app because the server already crashed
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("Unhandled rejection occured, shutting down");
+
+  server.cloce(() => {
+    process.exit(1);
+  })
+});
+
+/* 
+  process.exit( code )
+  Code: It can be either 0 or 1. 
+  0 means end the process without any kind of failure and 
+  1 means end the process with some failure.
+*/
