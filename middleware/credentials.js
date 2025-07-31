df36ac5217a5f3f0e270f362e81f1b39c3962e94
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-31 22:27:03 +0800

    Creates a credentials file that checks if allowedOrgins is allowed credentials is trusted

---
+++ middleware/credentials.js
@@ -0,0 +1,21 @@
+import allowedOrigins from "../config/allowedOrigins.js";
+
+const credentials = (req, res, next) => {
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Controll-Allow-Credentials", true);
+  }
+
+  next();
+};
+
+export default credentials;
+
+/* 
+
+- for the Client || browser it need to set the -
+- Access-Control-Allow-Credentials: true
+- to ensure that the header is set to trusted origin
+
+*/
