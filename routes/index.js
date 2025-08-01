cda84247ef26aba0200dedcceed4a4815f05f9bc
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-01 15:58:49 +0800

    Import router as loginRoute then export so it can be use for api endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 //auth
 import {router as registerRoute} from "./auth/register.js";
+import { router as loginRoute} from "./auth/login.js"
 
 
 export {
-  registerRoute
+  registerRoute,
+  loginRoute
 };
 
