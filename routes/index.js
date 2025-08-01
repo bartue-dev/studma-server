cfc82f3f6a0d5727ff61f69fd6b59a03119a4c03
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-01 21:13:05 +0800

    Import router as logoutRoute

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@
 import { router as registerRoute } from "./auth/register.js";
 import { router as loginRoute } from "./auth/login.js"
 import { router as refreshTokenRoute } from "./auth/refreshToken.js"
+import { router as logoutRoute } from "./auth/logout.js"
 
 
 export {
   registerRoute,
   loginRoute,
-  refreshTokenRoute
+  refreshTokenRoute,
+  logoutRoute
 };
 
