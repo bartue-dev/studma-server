f7ab0035ca9f360fa108a4dbc39e95716dd6115f
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-01 18:41:59 +0800

    Import router as refreshTokenRoute from refreshToken file

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 //auth
-import {router as registerRoute} from "./auth/register.js";
-import { router as loginRoute} from "./auth/login.js"
+import { router as registerRoute } from "./auth/register.js";
+import { router as loginRoute } from "./auth/login.js"
+import { router as refreshTokenRoute } from "./auth/refreshToken.js"
 
 
 export {
   registerRoute,
-  loginRoute
+  loginRoute,
+  refreshTokenRoute
 };
 
