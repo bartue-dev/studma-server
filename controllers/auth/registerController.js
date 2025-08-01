178565af1b039a0a0047255d201bbe7642c43274
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-01 12:13:01 +0800

    Creates a register controller allows user to register an account

---
+++ controllers/auth/registerController.js
@@ -0,0 +1,25 @@
+import asyncHandler from "express-async-handler";
+import bcrypt from "bcryptjs";
+import { accountMethods } from "../../model/queries/auth/authQueries.js";
+import CustomErr from "../../helper/customErr.js";
+
+export const register = asyncHandler(async(req, res, next) => {
+  const { firstname, lastname, username, password} = req.body;
+
+  console.log("REGISTER API")
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const registeredAccount = await accountMethods.registerAccount(firstname, lastname, username, hashPassword);
+
+  if (!registeredAccount) {
+    const err = new CustomErr("Cannot create an account", 400)
+    next(err)
+    return
+  }
+
+  res.status(201).json({
+    status: "Success",
+    message: "Acconut registered succesfully"
+  });
+});
