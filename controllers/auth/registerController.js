9cc18d9d4e0ac29e608c3abca3ead12eed2a9635
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-01 15:55:31 +0800

    Remove console log

--- controllers/auth/registerController.js
+++ controllers/auth/registerController.js
@@ -6,8 +6,6 @@ import CustomErr from "../../helper/customErr.js";
 export const register = asyncHandler(async(req, res, next) => {
   const { firstname, lastname, username, password} = req.body;
 
-  console.log("REGISTER API")
-
   const hashPassword = await bcrypt.hash(password, 10);
 
   const registeredAccount = await accountMethods.registerAccount(firstname, lastname, username, hashPassword);
