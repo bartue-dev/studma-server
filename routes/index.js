64efbfb0be705c15372f8194787b403bc71b6718
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-03 22:46:20 +0800

    Import router as studentRoute

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,14 @@ import { router as loginRoute } from "./auth/login.js"
 import { router as refreshTokenRoute } from "./auth/refreshToken.js"
 import { router as logoutRoute } from "./auth/logout.js"
 
+//api
+import { router as studentRoute} from "./api/student.js"
 
 export {
   registerRoute,
   loginRoute,
   refreshTokenRoute,
-  logoutRoute
+  logoutRoute,
+  studentRoute
 };
 
