486ad64eaa0ec3dff77478945e55ef8ccb29f59b
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-03 23:48:26 +0800

    Import getAllStudent controller and input it as get request

--- routes/api/student.js
+++ routes/api/student.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { createStudent, getStudent } from "../../controllers/api/studentController.js"; 
+import { createStudent, getStudent, getAllStudent } from "../../controllers/api/studentController.js"; 
 const router = Router();
 
-router.post("/", createStudent)
+router.route("/")
+  .post(createStudent)
+  .get(getAllStudent)
+
 router.route("/:studentId")
   .get(getStudent)
 
