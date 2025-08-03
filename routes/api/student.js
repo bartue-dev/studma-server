7471bf0ae9d111c8bb82524a4db388cba3c9476f
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-03 23:04:42 +0800

    Import getStudent controller and input it to have a get request with studentId params

--- routes/api/student.js
+++ routes/api/student.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createStudent } from "../../controllers/api/studentController.js"; 
+import { createStudent, getStudent } from "../../controllers/api/studentController.js"; 
 const router = Router();
 
 router.post("/", createStudent)
-
+router.route("/:studentId")
+  .get(getStudent)
 
 
 export { router }
