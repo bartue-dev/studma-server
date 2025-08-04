d36e79b2183e31013e736cc2ffc6af61625c3de8
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-05 06:06:35 +0800

    Import getGrade to have a get request with gradeId params

--- routes/api/grade.js
+++ routes/api/grade.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { createGrade, getAllGrade } from "../../controllers/api/gradeController.js";
+import { createGrade, getAllGrade, getGrade } from "../../controllers/api/gradeController.js";
 const router = Router();
 
 router.route("/:studentId")
   .post(createGrade)
+
+router.route("/:gradeId")
+  .get(getGrade)
 
 router.route("/")
   .get(getAllGrade)
