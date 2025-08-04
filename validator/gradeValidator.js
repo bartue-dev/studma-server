dad114fac6265cff954ac0368222f4f0e4243395
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-05 06:07:06 +0800

    CReates a validateGradeId. Validator the param gradeId before proceeding to create the data

--- validator/gradeValidator.js
+++ validator/gradeValidator.js
@@ -9,3 +9,9 @@ export const validateCreateGrade = [
     .exists().withMessage("studentId doesnt exist")
     .isUUID().withMessage("studentId is not a valid UUID")
 ]
+
+export const validateGradeId = [
+  param("gradeId")
+    .exists().withMessage("gradeId doesn't exist")
+    .isUUID().withMessage("gradeId is not a valid UUID")
+]
