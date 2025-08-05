a764d11b6d17d1e9bd412ef6f6e75a0aebe22b56
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-05 21:11:28 +0800

    Import the getAllSection controller to have a get request

--- routes/api/section.js
+++ routes/api/section.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { createSection } from "../../controllers/api/sectionController.js";
+import { createSection, getAllSection } from "../../controllers/api/sectionController.js";
 const router = Router();
 
 router.route("/:studentId")
   .post(createSection)
+
+router.route("/")
+  .get(getAllSection)
 
 export { router }
