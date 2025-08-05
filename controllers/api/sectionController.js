2b076a0680ff2ba319c92e4d8e33a2883bf3bfe9
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-05 20:46:51 +0800

    Creates a sectionController. Right know it has a createSection. Able to create a section data associated with the student and teacher

---
+++ controllers/api/sectionController.js
@@ -0,0 +1,34 @@
+import asyncHandler from "express-async-handler";
+import { validateCreateSection } from "../../validator/sectionValidator.js";
+import { validationResult } from "express-validator";
+import { sectionMethods } from "../../model/queries/api/sectionQueries.js";
+import CustomErr from "../../helper/customErr.js";
+
+export const createSection = [validateCreateSection, asyncHandler(async(req, res, next) => {
+  const { section } = req.body;
+  const { studentId } = req.params;
+  const teacherId = req.user.id;
+  const validationRes = validationResult(req);
+
+  if (!validationRes.isEmpty()) {
+    return res.status(400).json({
+      status: "Failed",
+      message: "Failed to create section, validation error",
+      error: validationRes.array()
+    });
+  }
+
+  const sectionData = await sectionMethods.createSection(section, studentId, teacherId);
+
+  if (!sectionData) {
+    const err = CustomErr("Failed to create section data, custom error", 400) 
+    next(err);
+    return;
+  }
+
+  res.status(200).json({
+    status: "Success",
+    message: "Section created succesfully",
+    sectionData: sectionData
+  })
+})];
