5a0b7cf5aea8386160e94efd8b45a0538d7e94ef
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-06 19:31:15 +0800

    Creates an validateUpdateAttendanceDate. Validates the body and params before saving to db. Added an isIn method which only allows PRESENT, ABSENT, LATE and EXCUSE data in status field

--- validator/attenceDateValidator.js
+++ validator/attenceDateValidator.js
@@ -1,15 +1,31 @@
 import { body, param } from "express-validator";
 
 const isEmpty = "must not be empty";
+const status = ["PRESENT", "ABSENT", "LATE", "EXCUSE"];
 
 export const validateCreateAttenceDate = [
   body("date").trim()
     .notEmpty().withMessage(`Attendance date ${isEmpty}`)
-    .isDate().withMessage("Invalid Date")
+    .isDate().withMessage("Invalid Date"),
+  body("status").trim()
+    .notEmpty().withMessage(`Status ${isEmpty}`)
+    .isIn(status).withMessage("Status must be one of: PRESENT, ABSENT, LATE, EXCUSE")
 ]
 
 export const validateAttendanceDateId = [
   param("attendanceDateId")
     .exists().withMessage("attendanceDateId doesnt exist")
     .isUUID().withMessage("attendanceDateId is not a valid UUID")
+]
+
+export const validateUpdateAttendanceDate = [
+  param("attendanceDateId")
+    .exists().withMessage("attendanceDateId doesnt exist")
+    .isUUID().withMessage("attendanceDateId is not a valid UUID"),
+  body("date").trim()
+    .notEmpty().withMessage(`Attendance date ${isEmpty}`)
+    .isDate().withMessage("Invalid Date"),
+  body("status").trim()
+    .notEmpty().withMessage(`Status ${isEmpty}`)
+    .isIn(status).withMessage("Status must be one of: PRESENT, ABSENT, LATE, EXCUSE")
 ]
