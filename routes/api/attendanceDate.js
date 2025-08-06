4648129f94fe1622be3441be96ca2300f8e675c1
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-06 18:24:23 +0800

    Import getAttendanceDate to have a get request with attendanceDateId params

--- routes/api/attendanceDate.js
+++ routes/api/attendanceDate.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { createAttendanceDate, getAllAttendanceDate } from "../../controllers/api/attendanceDateController.js";
+import { createAttendanceDate, getAllAttendanceDate, getAttendanceDate } from "../../controllers/api/attendanceDateController.js";
 const router = Router();
 
-router.route("/:studentId")
+router.route("/student/:studentId")
   .post(createAttendanceDate)
 
 router.route("/")
   .get(getAllAttendanceDate)
+
+router.route("/:attendanceDateId")
+  .get(getAttendanceDate)
 
 export { router }
