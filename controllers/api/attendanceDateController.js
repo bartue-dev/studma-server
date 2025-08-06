80a89c48001b54feabe408aa1e583c8eb301316f
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-06 14:43:11 +0800

    Creates an attendanceDateController and for now it has a createAttendanceDate that able to create a attendanceDate data

---
+++ controllers/api/attendanceDateController.js
@@ -0,0 +1,34 @@
+import asyncHandler from "express-async-handler";
+import { validateCreateAttenceDate } from "../../validator/attenceDateValidator.js";
+import { validationResult } from "express-validator";
+import { attendanceDateMethods } from "../../model/queries/api/attendanceDateQueries.js";
+import CustomErr from "../../helper/customErr.js";
+
+export const createAttendanceDate = [validateCreateAttenceDate, asyncHandler(async(req, res, next) => {
+  const {date , status } = req.body;
+  const { studentId } = req.params;
+  const teacherId = req.user.id;
+  const validationRes = validationResult(req);
+
+  if (!validationRes.isEmpty()) {
+    return res.status(400).json({
+      status: "Failed",
+      message: "Failed to create attendance date, validation error",
+      error: validationRes.array()
+    });
+  }
+
+  const attendanceDateData = await attendanceDateMethods.createAttendaceDate(date, status, studentId, teacherId)
+
+  if (!attendanceDateData) {
+    const err = new CustomErr("Failed to create attendance date, custom error" ,400)
+    next(err);
+    return;
+  }
+
+  res.status(200).json({
+    status: "Success",
+    message: "Attendance date created successfully",
+    attendanceDateData: attendanceDateData
+  })
+})]
