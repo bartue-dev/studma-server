078a5fcc4ca1fc2d4078cd38c18592ec3b07883f
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-06 18:24:50 +0800

    Add student to the endpoint before studentId params

--- routes/api/grade.js
+++ routes/api/grade.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { createGrade, deleteGrade, getAllGrade, getGrade, updateGrade } from "../../controllers/api/gradeController.js";
 const router = Router();
 
-router.route("/:studentId")
+router.route("/student/:studentId")
   .post(createGrade)
 
 router.route("/:gradeId")
