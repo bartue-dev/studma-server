3010baea39cefa756ef0cd0e73e6f44d96a6f948
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-13 23:45:06 +0800

    Edit the logout post request to get request

--- routes/auth/logout.js
+++ routes/auth/logout.js
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { logout } from "../../controllers/auth/logoutController.js";
 const router = Router();
 
-router.get("/", logout)
+router.post("/", logout)
 
 export { router }
