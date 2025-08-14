12d2ddccfb75ef1d99021a370e0ab22bbc0be27c
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-14 23:51:41 +0800

    Remove the gradeRoute sectionRoute and batchRoute

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,7 @@ import { router as logoutRoute } from "./auth/logout.js"
 
 //api
 import { router as studentRoute} from "./api/student.js"
-import { router as gradeRoute } from "./api/grade.js"
-import { router as sectionRoute } from "./api/section.js"
 import { router as attendanceDateRoute } from "./api/attendanceDate.js"
-import { router as batchRoute } from "./api/batch.js"
 
 export {
   registerRoute,
@@ -17,9 +14,6 @@ export {
   refreshTokenRoute,
   logoutRoute,
   studentRoute,
-  gradeRoute,
-  sectionRoute,
   attendanceDateRoute,
-  batchRoute
 };
 
